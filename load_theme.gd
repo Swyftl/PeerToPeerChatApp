e3af275230988b949bd0c6cd49a4d950f7f5f5a3
Swyftl <juniorjonswift@gmail.com>
2025-09-22 22:03:22 +0100

    Now saves the path to the selected theme

--- load_theme.gd
+++ load_theme.gd
@@ -1,5 +1,21 @@
 extends Button
 
+func _ready() -> void:
+	_loadTheme()
+	
+func _loadTheme():
+	var config := ConfigFile.new()
+	var save_path = "user://theme.cfg"
+	
+	var err = config.load(save_path)
+	if err != OK:
+		print("no save was detected for a theme")
+	
+	var theme_path = config.get_value("theme", "path", "")
+	
+	_changeTheme(theme_path)
+	
+
 func _on_pressed() -> void:
 	var filePicker = FileDialog.new()
 	
@@ -10,7 +26,19 @@ func _on_pressed() -> void:
 	add_child(filePicker)
 	filePicker.visible = true
 	
-	filePicker.file_selected.connect(_fileSelected)
+	filePicker.file_selected.connect(_changeTheme)
 	
-func _fileSelected(file):
-	$"..".theme = load(file)
+func _changeTheme(file):
+	if file != "":
+		$"..".theme = load(file)
+		_saveTheme(file)
+	
+func _saveTheme(file):
+	var config := ConfigFile.new()
+	var savePath = "user://theme.cfg"
+	
+	config.set_value("theme", "path", file)
+	
+	var err = config.save(savePath)
+	if err != OK:
+		push_error("Failed to save theme")
