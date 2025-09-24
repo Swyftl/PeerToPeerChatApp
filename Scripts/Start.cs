1f0cca3aaf95084d710b80c6d71035147fda22a3
Swyftl <juniorjonswift@gmail.com>
2025-09-24 22:03:43 +0100

    Program now checks for updates and tells the user about them.

--- Scripts/Start.cs
+++ Scripts/Start.cs
@@ -1,14 +1,56 @@
 using Godot;
+using Octokit;
 
 namespace PeerToPeerChatApp;
 
 public partial class Start : Control
 {
     private LineEdit _usernameInput;
+    private string latestClientVersion;
     
     public override void _Ready()
     {
         _usernameInput = GetNode<LineEdit>("LineEdit");
+        // Check for updates on the client side
+
+        if (CheckForUpdates())
+        {
+            var updatePopup = new AcceptDialog();
+
+            updatePopup.OkButtonText = "I Understand";
+            updatePopup.DialogText =
+                $"And update to version {latestClientVersion} has been found, please update when you can.";
+            updatePopup.Title = "Update Found";
+            
+            AddChild(updatePopup);
+            updatePopup.Show();
+        }
+    }
+
+    private bool CheckForUpdates()
+    {
+        var client = new GitHubClient(new ProductHeaderValue("PeerToPeerChatApp"));
+        var releases = client.Repository.Release.GetLatest("Swyftl", "PeerToPeerChatApp");
+
+        var latest = releases.Result;
+        
+        GD.Print($"The latest release was tagged at {latest.TagName} and is called {latest.Name}");
+        GD.Print("Checking for updates...");
+
+        var latestVersion = latest.TagName.Substring(1, latest.TagName.Length-1).Trim();
+        
+        latestClientVersion = latestVersion;
+
+        if (latestVersion != ProjectSettings.GetSetting("application/config/version").ToString())
+        {
+            GD.Print("An update was found, prompting the user now!");
+            return true;
+        }
+        else
+        {
+            GD.Print("The latest version is up to date!");
+            return false;
+        }
     }
     
     public void _on_button_pressed()
